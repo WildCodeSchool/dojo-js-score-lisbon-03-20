ec6f86d2b149e36765dbeab02d1d80b07f9df86f
Arnaud Duhé <anro010@hotmail.com>
2020-04-21 11:56:06 +0100

    both tests run OK

--- podium/podium.js
+++ podium/podium.js
@@ -1,5 +1,36 @@
-function podium(scores, player, modifier) {
-    // write your code here
+const scores = [
+    {
+        "player": "Maggie",
+        "score": 762
+    },
+    {
+        "player": "Annie",
+        "score": 102
+    },
+    {
+        "player": "Sanchez",
+        "score": 882
+    },
+    {
+        "player": "Dee",
+        "score": 609
+    }
+];
+
+function podium(scores) {
+    const nom = scores.sort((function (a, b) { return b.score - a.score }))
+    let selection = nom.slice(0, 3)
+    console.log(selection)
+    return selection
 }
+podium(scores)
+
+
+//we write a function that passes an array of objects
+//we use sort() method to sort out objects by the score 
+//(points.sort(function(a, b){return a-b})
+//we use slice() method to save 3 objects with the best scores 
+//(array.slice(start, end))
+//we return an array with 3 selected objects
 
 module.exports = podium;
--- updateScore/updateScore.js
+++ updateScore/updateScore.js
@@ -1,5 +1,38 @@
+const scores = [
+    {
+        "player": "Maggie",
+        "score": 762
+    },
+    {
+        "player": "Annie",
+        "score": 102
+    },
+    {
+        "player": "Sanchez",
+        "score": 882
+    },
+    {
+        "player": "Dee",
+        "score": 609
+    }
+];
+
 function updateScore(scores, player, modifier) {
     // write your code here
+    const who = scores.findIndex(el => el.player === player)
+    const oldScore = scores[who].score
+    scores[who].score = oldScore + modifier
+    return scores
 }
 
+updateScore(scores, "Sanchez", 100)
+
+console.log(scores)
+
 module.exports = updateScore;
+
+//the function will pass an object as parameter
+//find index of player in the array
+//get his score
+//apply modification of his score
+//return scores (arraw) updated
