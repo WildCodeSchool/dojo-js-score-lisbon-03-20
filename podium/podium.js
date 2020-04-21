83ea4e2846727c4973f1df21ee19b200710e685e
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-04-21 10:46:14 +0100

    two passed

--- podium/podium.js
+++ podium/podium.js
@@ -1,5 +1,15 @@
-function podium(scores, player, modifier) {
-    // write your code here
+// function myFunction() {
+// points.sort(function (a, b) { return b - a });
+// Separate first three top values
+//arr.sort( function ( a, b ) { return b.date - a.date; } );
+
+function podium(scores) {
+  scores.sort(
+    (firstPerson, secondPerson) => secondPerson.score - firstPerson.score,
+  );
+
+  const scoreTop = scores.slice(0, 3);
+  return scoreTop;
 }
 
 module.exports = podium;
--- podium/podium.test.js
+++ podium/podium.test.js
@@ -1,24 +1,23 @@
-const updateScore = require("./podium");
-const scores = require("../scores");
+const updateScore = require('./podium');
+const scores = require('../scores');
 
 const expectedResult = [
-    {
-        "player": "Sanchez",
-        "score": 982
-    },
-    {
-        "player": "Maggie",
-        "score": 762
-    },
-    {
-        "player": "Dee",
-        "score": 609
-    }
+  {
+    player: 'Sanchez',
+    score: 882,
+  },
+  {
+    player: 'Maggie',
+    score: 762,
+  },
+  {
+    player: 'Dee',
+    score: 609,
+  },
 ];
 
-
-describe("Podium", () => {
-    it("returns the correct result", () => {
-        expect(updateScore(scores)).toEqual(expectedResult);
-    });
+describe('Podium', () => {
+  it('returns the correct result', () => {
+    expect(updateScore(scores)).toEqual(expectedResult);
+  });
 });
