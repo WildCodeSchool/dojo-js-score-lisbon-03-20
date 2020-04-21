c7723781d5efefbeb0f50d16d2c1278f53743708
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-04-21 10:56:32 +0100

    group 2 solutions

--- podium/podium.js
+++ podium/podium.js
@@ -1,5 +1,37 @@
-function podium(scores, player, modifier) {
-    // write your code here
+
+function podium(scores) {
+    let sorted = scores.sort((a,b) => {
+        return  b.score - a.score 
+    })
+    
+    return sorted.splice(0, 3);
 }
 
+ const scores = [
+     {
+       "player": "Maggie",
+       "score": 762
+     },
+     {
+       "player": "Annie",
+       "score": 102
+     },
+     {
+       "player": "Sanchez",
+       "score": 882
+     },
+     {
+       "player": "Dee",
+       "score": 609
+     }
+   ];
+
+   podium(scores);
+
+
+
 module.exports = podium;
+
+//iterate the array 
+//find the 3 highest scores
+//return the objects with the highest scores
--- updateScore/updateScore.js
+++ updateScore/updateScore.js
@@ -1,5 +1,37 @@
 function updateScore(scores, player, modifier) {
-    // write your code here
+    const scoreFilter = scores.map(element => {
+        if(element.player === player){
+            element.score += modifier
+        }
+        return element
+    })
+    return scoreFilter;
 }
 
+const scores = [
+    {
+      "player": "Maggie",
+      "score": 762
+    },
+    {
+      "player": "Annie",
+      "score": 102
+    },
+    {
+      "player": "Sanchez",
+      "score": 882
+    },
+    {
+      "player": "Dee",
+      "score": 609
+    }
+  ];
+  
+  updateScore(scores, "Sanchez", 100);
+
 module.exports = updateScore;
+
+//iterate the array scores
+//find the object player
+//change the score of object player by modifier
+//return the updated scores array 
